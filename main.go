a1f1c373bd20c2b566fa8b6860e00ae23799a527
Michael Dockter <michael@dockter.com>
2017-07-11 17:09:55 -0400

    Working chunking

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/go-ozzo/ozzo-routing"
 )
@@ -9,10 +11,36 @@ import (
 func main() {
 	router := routing.New()
 
+	// Hello world.
+
 	router.Get("/", func(c *routing.Context) error {
 		return c.Write("Hello world")
 	})
 
+	// Streaming.
+
+	router.Get("/streaming", func(c *routing.Context) error {
+		// Observation: This buffers into large chunks before sending.
+
+		// Need to cast http.ResponseWriter as a "flusher".
+
+		w := c.Response
+		flusher, ok := w.(http.Flusher)
+		if !ok {
+			panic("expected http.ResponseWriter to be an http.Flusher")
+		}
+
+		// Stream the response body.  Note: loop will not stop.
+
+		ticker := time.NewTicker(time.Millisecond * 500)
+		for aTime := range ticker.C {
+			c.Write([]byte(aTime.String()))
+			flusher.Flush()
+		}
+
+		return c.Write(">>> Shouldn't get here. <<<")
+	})
+
 	http.Handle("/", router)
-	http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
