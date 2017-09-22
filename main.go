fe9d4336a48ba7e066ef759bc4d22093a5ac71a9
Michael Dockter <michael@dockter.com>
2017-09-22 10:02:07 -0400

    Added /api

--- main.go
+++ main.go
@@ -6,10 +6,37 @@ import (
 	"time"
 
 	"github.com/go-ozzo/ozzo-routing"
+	"github.com/go-ozzo/ozzo-routing/access"
+	"github.com/go-ozzo/ozzo-routing/content"
+	"github.com/go-ozzo/ozzo-routing/fault"
+	"github.com/go-ozzo/ozzo-routing/slash"
 )
 
 func main() {
 	router := routing.New()
+
+	router.Use(
+		// all these handlers are shared by every route
+		access.Logger(log.Printf),
+		slash.Remover(http.StatusMovedPermanently),
+		fault.Recovery(log.Printf),
+	)
+
+	// serve RESTful APIs
+	api := router.Group("/api")
+	api.Use(
+		// these handlers are shared by the routes in the api group only
+		content.TypeNegotiator(content.JSON, content.XML),
+	)
+	api.Get("/users", func(c *routing.Context) error {
+		return c.Write("user list")
+	})
+	api.Post("/users", func(c *routing.Context) error {
+		return c.Write("create a new user")
+	})
+	api.Put(`/users/<id:\d+>`, func(c *routing.Context) error {
+		return c.Write("update user " + c.Param("id"))
+	})
 
 	// Hello world.
 
